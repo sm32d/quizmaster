3a94ac17f605e8a00142db62fe72b184ce54a5f3
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-06 22:14:57 +0800

    Fixed the issue where inserting user won't return user id

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -1,66 +1,65 @@
 package controllers
 
 import (
-    "quizmaster/app/models"
-    "quizmaster/app/services"
-    "github.com/gofiber/fiber/v2"
-    "github.com/go-playground/validator/v10"
-    "go.mongodb.org/mongo-driver/mongo"
+	"quizmaster/app/models"
+	"quizmaster/app/services"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type GetUserByEmailRequest struct {
-    Email string `json:"email" validate:"required,email"`
+	Email string `json:"email" validate:"required,email"`
 }
 
 func GetUserByEmailHandler(client *mongo.Client, email string) (*models.User, error) {
-    // Use the services function to fetch the user by email
-    user, err := services.GetUserByEmail(client, email)
-    if err != nil {
-        return nil, err
-    }
+	// Use the services function to fetch the user by email
+	user, err := services.GetUserByEmail(client, email)
+	if err != nil {
+		return nil, err
+	}
 
-    return user, nil
+	return user, nil
 }
 
 // CreateUserHandler handles the creation of a new user.
 func CreateUserHandler(c *fiber.Ctx, client *mongo.Client) error {
-    var user models.User
-    if err := c.BodyParser(&user); err != nil {
-        return c.Status(fiber.StatusBadRequest).SendString("Bad request")
-    }
+	var user models.User
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Bad request")
+	}
 
-    // Check if a user with the same email already exists
-    existingUser, err := services.GetUserByEmail(client, user.Email)
-    if err != nil {
-        return err // Handle the error, such as a database error
-    }
-    if existingUser != nil {
-        // User with the same email already exists, return an error response
-        return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-            "message": "User with this email already exists",
-        })
-    }
+	// Check if a user with the same email already exists
+	existingUser, err := services.GetUserByEmail(client, user.Email)
+	if err != nil {
+		return err // Handle the error, such as a database error
+	}
+	if existingUser != nil {
+		// User with the same email already exists, return an error response
+		return c.JSON(existingUser)
+	}
 
-    // Use the InsertUser function from the services package
-    err = services.InsertUser(client, user)
-    if err != nil {
-        return err
-    }
-
-    return c.JSON(user)
+	// Use the InsertUser function from the services package
+	dbId, err := services.InsertUser(client, user)
+	if err != nil {
+		return err
+	}
+    user.ID = dbId
+	return c.JSON(user)
 }
 
 func ParseAndValidateGetUserByEmailRequest(c *fiber.Ctx) (*GetUserByEmailRequest, error) {
-    var request GetUserByEmailRequest
-    validate := validator.New()
+	var request GetUserByEmailRequest
+	validate := validator.New()
 
-    if err := c.BodyParser(&request); err != nil {
-        return nil, err
-    }
+	if err := c.BodyParser(&request); err != nil {
+		return nil, err
+	}
 
-    if err := validate.Struct(request); err != nil {
-        return nil, err
-    }
+	if err := validate.Struct(request); err != nil {
+		return nil, err
+	}
 
-    return &request, nil
+	return &request, nil
 }
--- app/services/user_service.go
+++ app/services/user_service.go
@@ -1,48 +1,54 @@
 package services
 
 import (
-    "context"
-    "log"
-    "time"
-    "quizmaster/app/models"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
+	"context"
+	"log"
+	"quizmaster/app/models"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // GetUserByEmail retrieves a user by their email from the MongoDB database.
 func GetUserByEmail(client *mongo.Client, email string) (*models.User, error) {
-    // Access the MongoDB collection containing user data
-    collection := client.Database("quizmaster").Collection("users")
+	// Access the MongoDB collection containing user data
+	collection := client.Database("quizmaster").Collection("users")
 
-    // Define a filter to find the user by email
-    filter := bson.M{"email": email}
+	// Define a filter to find the user by email
+	filter := bson.M{"email": email}
 
-    // Create a context for the database operation
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // A 5-second timeout
-    defer cancel() // Cancel the context when the function returns
+	// Create a context for the database operation
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // A 5-second timeout
+	defer cancel()                                                          // Cancel the context when the function returns
 
-    // Find one document that matches the filter
-    var user models.User
-    err := collection.FindOne(ctx, filter).Decode(&user)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return nil, nil // No matching user found, return nil
-        }
-        return nil, err // Handle other errors, e.g., database connection issues
-    }
+	// Find one document that matches the filter
+	var user models.User
+	err := collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil // No matching user found, return nil
+		}
+		return nil, err // Handle other errors, e.g., database connection issues
+	}
 
-    return &user, nil
+	return &user, nil
 }
 
 // InsertUser inserts a user into the database.
-func InsertUser(client *mongo.Client, user models.User) error {
-    collection := client.Database("quizmaster").Collection("users")
+func InsertUser(client *mongo.Client, user models.User) (primitive.ObjectID, error) {
+	collection := client.Database("quizmaster").Collection("users")
 
-    _, err := collection.InsertOne(context.TODO(), user)
-    if err != nil {
-        log.Println("Error inserting user:", err)
-        return err
-    }
+	// Create a context for the database operation
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // A 5-second timeout
+	defer cancel()
 
-    return nil
+	insertedUser, err := collection.InsertOne(ctx, user)
+	if err != nil {
+		log.Println("Error inserting user:", err)
+		return primitive.NilObjectID, err
+	}
+
+	return insertedUser.InsertedID.(primitive.ObjectID), nil
 }
