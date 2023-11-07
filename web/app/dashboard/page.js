6b5c3129cd89a02aebccd9c7acb3850fbabaa3ca
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-07 21:23:04 +0800

    Removed the redirect as it would be handled by middleware now

--- web/app/dashboard/page.js
+++ web/app/dashboard/page.js
@@ -1,11 +1,7 @@
 import { options } from "../api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
 const Dashboard = async () => {
   const session = await getServerSession(options);
-  if (!session) {
-    redirect("/");
-  }
   return <div>dashboard</div>;
 };
 
