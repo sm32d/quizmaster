cccef18daa90abdcb0413862106c0d10598406a7
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-08 20:04:36 +0800

    Changed the base bg colour to be lighter

--- web/app/layout.js
+++ web/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <AuthProvider>
           <Nav />
-          <div className="bg-base-200">
+          <div className="bg-base-100">
           {children}
           </div>
           <Footer />
