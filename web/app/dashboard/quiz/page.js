3d7371c3e7a3dd71243f0d565eed7d275030a28f
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-08 22:59:46 +0800

    Removed fetch quizzes as it is moved to dashboard

--- web/app/dashboard/quiz/page.js
+++ web/app/dashboard/quiz/page.js
@@ -1,23 +1,5 @@
-async function fetchQuizzes() {
-  const backendUri = process.env.BACKEND_URI;
-  try {
-    const response = await fetch(`${backendUri}/api/quizzes`, {
-      cache: "no-store",
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data: ", error);
-    return null;
-  }
-}
 
 const Quiz = async () => {
-  const quizzes = await fetchQuizzes();
-  console.log(quizzes);
   return <main>test</main>;
 };
 
