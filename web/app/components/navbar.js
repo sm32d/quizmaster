8f430192b798f87612cec27d2b721f8aa6f24f5d
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-09 12:50:51 +0800

    Changed menus to be closable by clicking the same button used to open it

--- web/app/components/navbar.js
+++ web/app/components/navbar.js
@@ -8,8 +8,8 @@ const navbar = async () => {
   const session = await getServerSession(options);
   return (
     <div className="navbar bg-base-100">
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost btn-circle">
+      <details className="dropdown">
+        <summary tabIndex={0} className="btn btn-ghost btn-circle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -24,7 +24,7 @@ const navbar = async () => {
               d="M4 6h16M4 12h16M4 18h7"
             />
           </svg>
-        </label>
+        </summary>
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
@@ -42,7 +42,7 @@ const navbar = async () => {
             <a>About</a>
           </li>
         </ul>
-      </div>
+      </details>
       <div className="flex-1">
         <Link href="/" className="btn btn-ghost normal-case text-xl">
           QuizMaster
@@ -50,12 +50,12 @@ const navbar = async () => {
       </div>
       <div className="flex-none px-4">
         {session ? (
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <details className="dropdown dropdown-end">
+            <summary tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <img src={session?.user?.image} />
               </div>
-            </label>
+            </summary>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
@@ -73,7 +73,7 @@ const navbar = async () => {
                 <SignOut />
               </li>
             </ul>
-          </div>
+          </details>
         ) : (
           <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
