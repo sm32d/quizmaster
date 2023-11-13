b97779eb7c9ab9860c6c4226c2488bedfe62d8f2
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-13 12:59:47 +0800

    Added light and dark themes from daisyUI

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#a81952",
+
+          secondary: "#cee809",
+
+          accent: "#0e8234",
+
+          neutral: "#28202c",
+
+          "base-100": "#ededee",
+
+          info: "#6cc1ef",
+
+          success: "#6de9c0",
+
+          warning: "#f0db70",
+
+          error: "#e5345d",
+        },
+      },
+      {
+        dark: {
+          primary: "#81f48f",
+
+          secondary: "#ff6b66",
+
+          accent: "#e5647e",
+
+          neutral: "#302932",
+
+          "base-100": "#322d39",
+
+          info: "#76bee0",
+
+          success: "#4be7ca",
+
+          warning: "#eca241",
+
+          error: "#f44e6c",
+        },
+      },
+    ],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
