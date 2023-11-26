4c0a5babf8a3a1ea17a696652b54bf418e7bb40f
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2023-11-26 21:21:51 +0800

    Added more fields

--- app/models/question.go
+++ app/models/question.go
@@ -1,10 +1,13 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // Question represents a question in a quiz.
 type Question struct {
-	Text       string   `json:"text" bson:"text"`
-	Choices    []string `json:"choices" bson:"choices"`
-	Correct    string   `json:"correct" bson:"correct"`
-	Difficulty string   `json:"difficulty" bson:"difficulty"`
-	Section    string   `json:"section" bson:"section"`
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Text       string             `json:"text" bson:"text"`
+	Choices    []string           `json:"choices" bson:"choices"`
+	Correct    string             `json:"correct" bson:"correct"`
+	Difficulty string             `json:"difficulty" bson:"difficulty"`
+	Section    string             `json:"section" bson:"section"`
 }
--- app/models/quiz.go
+++ app/models/quiz.go
@@ -8,12 +8,14 @@ import (
 
 // Quiz represents a quiz in the application.
 type Quiz struct {
-	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Title      string             `json:"title" bson:"title"`
-	Sections   []string           `json:"sections" bson:"sections"`
-	Difficulty string             `json:"difficulty" bson:"difficulty"`
-	Questions  []Question         `json:"questions" bson:"questions"`
-	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
-	CreatedBy  string             `json:"created_by" bson:"created_by"`
+	ID                    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title                 string             `json:"title" bson:"title"`
+	Sections              []string           `json:"sections" bson:"sections"`
+	Difficulty            string             `json:"difficulty" bson:"difficulty"`
+	Questions             []Question         `json:"questions" bson:"questions"`
+	CollectEmail          bool               `json:"collect_email" bson:"collect_email"`
+	AllowMultipleAttempts bool               `json:"allow_multiple_attempts" bson:"allow_multiple_attempts"`
+	CreatedAt             time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt             time.Time          `json:"updated_at" bson:"updated_at"`
+	CreatedBy             string             `json:"created_by" bson:"created_by"`
 }
