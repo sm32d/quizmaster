70217ecc613d9d1efdcbb42398ff383f84646f6b
Dominik <github@sume.uk>
2024-06-30 21:57:11 +0800

    fixed the bug where the ui would crash upon pressing share quiz button

--- web/app/components/shareQuiz.tsx
+++ web/app/components/shareQuiz.tsx
@@ -6,17 +6,21 @@ const ShareQuiz = ({ quizId }: { quizId: string }) => {
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <button
         onClick={() => {
-          navigator.clipboard.writeText(
-            `${window.location.origin}/eu/${quizId}`,
-          );
+          const linkURI = `${window.location.origin}/eu/${quizId}`;
           (
-            document.getElementById("share_modal") as HTMLDialogElement
+            document.getElementById(
+              `share_${quizId}_modal`
+            ) as HTMLDialogElement
           ).showModal();
+          navigator.clipboard.writeText(linkURI);
         }}
       >
         <Share size={20} />
       </button>
-      <dialog id="share_modal" className="modal modal-bottom sm:modal-middle">
+      <dialog
+        id={`share_${quizId}_modal`}
+        className="modal modal-bottom sm:modal-middle"
+      >
         <div className="modal-box sm:border sm:border-dotted sm:border-neutral shadow-xl">
           <h3 className="font-bold text-lg">Copied 🎉🎉</h3>
           <p className="py-2">Link copied to your clipboard.</p>
