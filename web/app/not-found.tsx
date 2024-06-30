68fab30ceb4afadd58729b93a8e0bcdef0980b01
Dominik <github@sume.uk>
2024-06-30 21:27:56 +0800

    cosmetic changes

--- web/app/not-found.tsx
+++ web/app/not-found.tsx
@@ -3,20 +3,22 @@ import { MoodSadDizzy } from "tabler-icons-react";
 
 export default function NotFound() {
   return (
-    <div className="hero min-h-screen">
-          <div className="hero-content flex-col">
-            <div className="flex">
-              <div className="flex items-center px-1"><MoodSadDizzy size={30} /></div>
-              <h1 className="text-2xl font-bold px-1">Uh oh!</h1>
-            </div>
-            <span className="max-w-md text-center">
-              This link seems broken or you do not have permission to view the
-              requested content.
-            </span>
-            <Link href="/">
-              <button className="btn mt-4">Go Back</button>
-            </Link>
+    <div className="hero min-h-[92svh]">
+      <div className="hero-content flex-col">
+        <div className="flex">
+          <div className="flex items-center px-1">
+            <MoodSadDizzy size={30} />
           </div>
+          <h1 className="text-2xl font-bold px-1">Uh oh!</h1>
         </div>
+        <span className="max-w-md text-center">
+          This link seems broken or you do not have permission to view the
+          requested content.
+        </span>
+        <Link href="/">
+          <button className="btn mt-4">Go Back</button>
+        </Link>
+      </div>
+    </div>
   );
 }
